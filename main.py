da55829553637bc75267e9b0a7bc3baee0e5ae13
mao <1102836917@qq.com>
2017-08-10 18:44:57 +0800

    添加多个映射zhihu,love,/

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ import web
 from weixin_server.handle import Handle
 #url映射
 urls=(
-	'/hello','index',#根目录
-	'/aa','index',
+	'/zhihu','demo2',#根目录
+	'/love','demo3',
 	'/wx', 'Handle',#微信公众号消息接收地址
-	'/(.*)','demo',#匹配的内容回会传入到函数里
+	'/(.*)','demo1',#匹配的内容回会传入到函数里
 )
 app=web.application(urls,globals())
 
 class index(object):
 	"""docstring for index"""
 	def GET(self):
-		return "hello"
+		return "Hello,World"
 
-class demo(object):
+class demo1(object):
 	"""docstring for demo"""
 	def GET(self,name):
 		# render=web.template.frender("demo1/index.html")
 		return open(r'demo1/index.html','r').read()
+class demo2(object):
+	"""docstring for demo"""
+	def GET(self,name):
+		# render=web.template.frender("demo1/index.html")
+		return open(r'demo1/index2.html','r').read()
+class demo3(object):
+	"""docstring for demo"""
+	def GET(self,name):
+		# render=web.template.frender("demo1/index.html")
+		return open(r'demo1/heart.html','r').read()
 		
 if __name__ == '__main__':
 	app.run()
